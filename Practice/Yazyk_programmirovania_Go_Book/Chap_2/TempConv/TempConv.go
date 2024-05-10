e3d021816541d716ae0d0628a0b0297aa25246c0
Pasha <braslpaf@gmail.com>
2024-05-10 12:21:53 +0300

    TempConv.go

---
+++ Practice/Yazyk_programmirovania_Go_Book/Chap_2/TempConv/TempConv.go
@@ -0,0 +1,15 @@
+// Пакет tempconv выполняет вычисления температур
+// по Цельсию (Celsius) и по Фаренгейту (Fahrenheit),
+package tempconv
+
+type Celsius float64
+type Fahrenheit float64
+
+const (
+	AbsoluteZeroC Celsius = -273.15
+	FreezingC     Celsius = 0
+	BoilingC      Celsius = 100
+)
+
+func CToF(c Celsius) Fahrenheit { return Fahrenheit(c*9/5 + 32) }
+func FToF(f Fahrenheit) Celsius { return Celsius((f - 32) * 5 / 9) }
